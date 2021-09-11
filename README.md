1bc55e400e231db8823b2ba30653b1f43acc564c
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2021-09-12 00:56:23 +0200

    copied the code from Pinar. Added a messafe audio and changed the profile images

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
-# Project Name
-
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+# Project chatbot
+We developed a music bot which asks your mode and offers a song depending on its own mode ☺ 
+This project was done in pairs. And we experienced pair programming for the first time. 
+The main target was to practicing DOM, conditionals, functions and variables. 
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+We proceeded according to the given iterations. The first step was adding a start button to activate the greeting. After the users first input, we proceeded to the next question in turn.
+It changes the background color depending on the users input and offers alternative music styles. At the end of the conversation, it plays a song. When we finished the structure then we styled it.
+We wanted to keep it simple so we wouldn’t get confused at the beginning of our js learning.  
+If we had more time, the bot could have a better logical conversation with more functions.
 
 ## View it live
+https://djbot-katie-pinar.netlify.app/
 
-Have you deployed your project somewhere? Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+
+//Pinar and Katie 
+GitHub: pdetli and katiewu1
+
+
--- code/script.js
+++ code/script.js
@@ -1,47 +1,176 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
+const userInput = document.getElementById('user-input')
+const sendBtn = document.getElementById('send-btn')
+const nameForm = document.getElementById('name-form')
+const startButton = document.getElementById('startButton')
+const startPage = document.getElementById('startPage')
+const inputWrapper = document.getElementById('input-wrapper')
+const messageAudio = new Audio('./message-pop-alert.mp3')
 
-// Global variables, if you need any, declared here
-
-// Functions declared here
+//FUNCTIONS
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
+    console.log("Hello!")
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
           <p>${message}</p>
         </div>
-        <img src="assets/user.png" alt="User" />  
+        <img src="assets/fox.svg" alt="User" />  
       </section>
     `
   } else if (sender === 'bot') {
     chat.innerHTML += `
       <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
+        <img src="assets/alienMonster.svg" alt="Bot" />
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
       </section>
     `
   }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
+
+  //add a sound to each sending message
+  messageAudio.currentTime = 0 
+  messageAudio.play()
+// This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight
 }
 
-// Starts here
-const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
+//to update the option for music style Study
+const updateStudy = () => {
+  const selectStudy = document.getElementById('selectStudy')
+  if (selectStudy.value === 'brain') {
+    userReply(`Brain Power`)
+    setTimeout(playMusic, 1000)
+  } else if (selectStudy.value === 'relaxing') {
+    userReply(`Relaxing`)
+    setTimeout(playMusic, 1000)
+  } else if (selectStudy.value === 'focus') {
+    userReply(`Focus`)
+    setTimeout(playMusic, 1000)
+  } else {
+    alert('Not a valid option!')
+  }
 }
 
-// Set up your eventlisteners here
+//to update the option for music style Party
+const updateParty = () => {
+  const selectParty = document.getElementById('selectParty')
+  if (selectParty.value === 'ninety') {
+    userReply(`90’s party`)
+    setTimeout(playMusic, 1000)
+  } else if (selectParty.value === 'eighty') {
+    userReply(`80’s party`)
+    setTimeout(playMusic, 1000)
+  } else if (selectParty.value === 'millenium') {
+    userReply(`2000’s hits`)
+    setTimeout(playMusic, 1000)
+  } else {
+    alert('Not a valid option!')
+  }
+}
 
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+// QUESTIONS
+
+// show the first question after clicking to the start button 
+startButton.onclick = () => {
+  startPage.style.display = "none"
+  setTimeout(greeting, 1000)
+}
+
+// Question 1 
+const greeting = () => {
+  showMessage(`Hello stranger! What's your name?`, 'bot')
+}
+
+const userReply = (input) => {
+  showMessage(input, 'user')
+}
+
+sendBtn.addEventListener('click', () => {
+  userReply(userInput.value)
+  setTimeout(pickColor, 1000)
+})
+
+// Question 2 
+const pickColor = () => {
+  showMessage(`Nice to meet you ${userInput.value}. What is your favorite color?`, 'bot')
+  inputWrapper.innerHTML = `
+  <button id= "pinkBtn"> Pink </button>    
+  <button id= "greenBtn"> Green </button>
+  <button id= "blackBtn"> Black </button>
+  `
+  const pinkBtn = document.getElementById('pinkBtn')
+  const greenBtn = document.getElementById('greenBtn')
+  const blackBtn = document.getElementById('blackBtn')
+  pinkBtn.onclick = () => {
+    //when user click on the Pink button, it's going to change background color to Pink. 
+    document.body.style.backgroundColor = "pink";
+    userReply(`Pink`)                 //invoke userReply function with the input "Pink"
+    setTimeout(pickMusicStyle, 1000)  // After 1s invoke function pickMusicStyle
+  }
+  greenBtn.onclick = () => {
+    document.body.style.backgroundColor = "green";
+    userReply(`Green`)
+    setTimeout(pickMusicStyle, 1000)
+  }
+  blackBtn.onclick = () => {
+    document.body.style.backgroundColor = "black";
+    userReply(`Black`)
+    setTimeout(pickMusicStyle, 1000)
+  }
+}
+
+// Question 3 
+const pickMusicStyle = () => {
+  showMessage(`Nice color! Now choose your favorite music style for today.`, 'bot')
+  inputWrapper.innerHTML = `
+  <button id= "studyBtn"> Study </button>    
+  <button id= "partyBtn"> Party </button>
+  `
+  const studyBtn = document.getElementById('studyBtn')
+  const partyBtn = document.getElementById('partyBtn')
+  studyBtn.onclick = () => {
+    inputWrapper.innerHTML = ` 
+      <select id="selectStudy" onchange="updateStudy()" >
+        <option value="" selected disabled>👇 Select one...</option>
+        <option id= "brain" value="brain">Brain Power</option>
+        <option id= "relaxing" value="relaxing">Relaxing</option>
+        <option id= "focus" value="focus">Focus</option>
+      </select> 
+      `
+    userReply(`Study`)
+  }
+
+  partyBtn.onclick = () => {
+    inputWrapper.innerHTML = `
+      <select id="selectParty" onchange= "updateParty ()">
+        <option value="" selected disabled>👇 Select one...</option>
+        <option id= "ninety" value="ninety"> 90’s party </option>
+        <option id= "eighty" value="eighty">80’s party </option>
+        <option id= "millenium" value="millenium">2000’s hits</option>
+      </select>
+      `
+    userReply(`Party`)
+  }
+}
+
+// Question 4
+const playMusic = () => {
+  showMessage(`A good choice!`, 'bot')
+  setTimeout(() => showMessage(`But I will play this instead! 🤖`, 'bot'), 1000)
+  inputWrapper.innerHTML = `
+  <audio controls loop> 
+  <source src="./automatik.mp3" type= "audio/mp3"> Your browser does not support the audio tag. 
+  </audio>
+    ` 
+}
+
+// prevent page refresh
+nameForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+})
